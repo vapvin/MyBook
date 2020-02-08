dd413c0ee68ccb0d9cef7c627a6b23fcfa4e5eb2
vapvin <tpehdj12@daum.net>
2020-02-08 22:03:09 +0900

    Note Query Write

--- src/clientState.js
+++ src/clientState.js
@@ -1,3 +1,5 @@
+import { NOTE_FRAGMENT } from "./fragments";
+
 export const defaults = {
   notes: [
     {
@@ -33,10 +35,13 @@ export const typeDefs = [
 ];
 export const resolvers = {
   Query: {
-    notes: (_, args, { getCacheKey }) => {
-      const id = getCacheKey({ __typename: "Note", id: args.id });
-      console.log(id);
-      return null;
+    note: (_, variables, { cache }) => {
+      const id = cache.config.dataIdFromObject({
+        __typename: "Note",
+        id: variables.id
+      });
+      const note = cache.readFragment({ fragment: NOTE_FRAGMENT, id });
+      return note;
     }
   }
 };
