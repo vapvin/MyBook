3e8fd74c10d0af5b25e3789d2fdd59ed401de646
vapvin <tpehdj12@daum.net>
2020-02-08 21:58:19 +0900

    Add Schema use Graphql

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-
+import React from "react";
+import { Query } from "react-apollo";
+import { GET_NOTES } from "./queries";
 
 function App() {
   return (
-   
+    <div className="App">
+      <Query query={GET_NOTES}>{() => null}</Query>
+    </div>
   );
 }
 
--- src/clientState.js
+++ src/clientState.js
@@ -1,3 +1,27 @@
-export const defaults = {};
+export const defaults = {
+  notes: []
+};
 export const resolvers = {};
-export const typeDefs = {};
+export const typeDefs = [
+  `
+    schema {
+        query: Query
+        mutation: Mutation
+    }
+
+    type Query {
+        notes: [Note]!
+        note(id: Int!): Note
+    }
+
+    type Mutation {
+        createNote(title: String!, content: String!)
+        editNote(id: String!, title: String!, content: String!)
+    }
+    type Note{
+        id: Int!
+        title: String!
+        content: String!
+    }
+    `
+];
